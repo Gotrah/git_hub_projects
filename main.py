af97bbc9a79eff74cd99581cd50d422900287021
Alex <hartog.alex@gmail.com>
2023-04-22 17:52:24 +0200

    Version showing bar chart for repositories with links

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
+import requests
+import plotly.express as px
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Make an API call and check the response.
+url = "https://api.github.com/search/repositories"
+url += "?q=language:python+sort:stars+stars:>10000"
 
+headers = {"Accept": "application/vnd.github.v3+json"}
+r = requests.get(url, headers=headers)
+r.raise_for_status()
+print(f"Status code: {r.status_code}")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Process overal results
+response_dict = r.json()
+print(f"Complete results: {not response_dict['incomplete_results']}")
 
+# Process repository information
+repo_dicts = response_dict['items']
+repo_names = [repo_dict['name'] for repo_dict in repo_dicts]
+stars = [repo_dict['stargazers_count'] for repo_dict in repo_dicts]
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+repo_links, hover_texts = [], []
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for repo_dict in repo_dicts:
+    # Turn repo names into active links.
+    repo_name = repo_dict['name']
+    repo_url = repo_dict['html_url']
+    repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+    repo_links.append(repo_link)
+
+    # Build hover texts.
+    owner = repo_dict['owner']['login']
+    description = repo_dict['description']
+    hover_text = f"{owner}<br />{description}"
+    hover_texts.append(hover_text)
+
+# Make visualization
+title = "Most-Starred Python Projects on GitHub"
+labels = {'x': 'Repository', 'y': 'Stars'}
+fig = px.bar(x=repo_links, y=stars, title=title, labels=labels, hover_name=hover_texts)
+
+fig.update_layout(title_font_size=28, xaxis_title_font_size=20, yaxis_title_font_size=20)
+fig.show()
